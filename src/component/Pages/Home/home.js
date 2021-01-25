c7c196175d1f8bdeb3d4b77aaf14e69f333eb308
Oluwatomi Oyinkansolami   Atansuyi <tomisinatansuyi@gmail.com>
2021-01-25 11:02:44 +0100

    Update home.js

--- src/component/Pages/Home/home.js
+++ src/component/Pages/Home/home.js
@@ -1,21 +1,46 @@
 /*jshint esversion: 6 */
 
-import React, { Component } from 'react';
-import {} from 'react-background-slideshow';
-import { InfoConsumer } from '../Context';
+import React, { useState, useEffect } from 'react';
+import desktopImage from './../../assets/displayImage/desktopImage/homeImage.jpg';
+import mobileImage from './../../assets/displayImage/mobileImage/homeImage.jpg';
+import { Link } from 'react-router-dom';
 
 
-class Home extends Component {
-    render() {
+const Home = () => {
+    const imageUrl = useWindowWidth >= 650 ? desktopImage : mobileImage;
+    
         return(
-             <InfoConsumer>
-                 {value => {
-                     return <h2>{value}</h2>;
-                 }}
-            </InfoConsumer> 
+           <div className='landing' style={{ backgroundImage: `url(${imageUrl})` }}>
+                <div className='landing-content'>
+
+                    <p> SORT YOUR APPEARANCE WITH RAETANS </p>
+
+                    <Link to='/shop'>
+                        <button className='button' style={{ borderRadius: '10%' }} type="submit">
+                            SEE MORE
+                        </button>
+                    </Link>
+                    
+                </div>
+            </div>
          );
-    }
 }
+
+const useWindowWidth = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
+
+    return windowWidth;
+};
+
 
 export default Home; 
 
